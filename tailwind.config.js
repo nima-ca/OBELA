6541b32433307180c9963d802b774daab1a436dd
Nima nekouei <nekoueinima@gmail.com>
2022-02-15 18:42:20 +0330

    sass base file created.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,11 @@
 module.exports = {
   content: ['./src/**/*.{html,js}', './src/index.html'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'Roboto': ['Roboto']
+      }
+    },
   },
   plugins: [],
 }
